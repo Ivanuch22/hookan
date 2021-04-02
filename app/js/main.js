d494b83b41cacbaa4887f43a49485945ef08fbad
Ivan Kalynych <ivan.kalunuch12@gmail.com>
2021-04-02 20:01:39 +0300

    git add .

---
+++ app/js/main.js
@@ -0,0 +1,81 @@
+// new Swiper('.Proof__slider', {
+//     breakpoints: {
+//         320: {
+//             slidesPerView: 1,
+//         },
+//         1025: {
+//             slidesPerView: 3
+//         }
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     loop: true,
+//     spaceBetween: 30,
+// });
+
+function Details() {
+    $('.Form__popup-details').toggleClass('Active');
+    $('.Form__popup').toggleClass('Active');
+};
+function flavor() {
+    $('.Form__popup-flavor-block').toggleClass('Active');
+    $('.Form__popup-flavor').toggleClass('Active');
+};
+function Click() {
+    $('.Form').addClass('Active');
+    $('.footer__block').addClass('Active');
+    $('body').addClass('Active');
+};
+function exit() {
+    $('.Form').removeClass('Active');
+    $('.footer__block').removeClass('Active');
+    $('body').removeClass('Active');
+};
+
+
+//Калькулятор
+const time = document.getElementsByClassName('.ChooseHookahs__left-time');
+const checkBoxs = document.querySelectorAll('.ChooseHookahs__colum-checkbox');
+const range = document.getElementById('slider');
+const price = document.getElementsByClassName('.ChooseHookahs__rigth-price');
+const radios = [
+    {
+        name: '3 hookahs',
+        price: 100
+    },
+    {
+        name: '5 hookahs',
+        price: 200
+    },
+    {
+        name: '7 hookahs',
+        price: 300
+    },
+    {
+        name: '9 hookahs',
+        price: 400
+    },
+    {
+        name: '15 hookahs',
+        price: 500
+    }
+];
+
+for (let radio of checkBoxs) {
+    radio.onclick = function () {
+        takeActiveRadio(radio);
+    };
+};
+const takeActiveRadio = currentActive => {
+    const dataAtribute = currentActive.dataset.name;
+    const currentRadio = radios.find(radio => radio.name === dataAtribute);
+    const currentPrice = currentRadio.price;
+    console.log(currentPrice);
+};
+
+// const Time = () => {
+//     time.inerrHTML = ` ${range.value} hour`;
+// };
+// Time();
